155bd280df7d369253ce45d7d11ae6da2ef70285
will H <wustracean@gmail.com>
2026-07-01 13:52:08 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use std::time::{Instant,Duration};
+// various imports needed to establish the righ architecture
+
+pub type task<T> = Box<dyn FnOnce() -> T + Send + 'static>; // 
 
 fn main() {
 
